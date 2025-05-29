0270fd363ffa6c8e3a418f734b005ad2351336ac
Marcos Tavares <mdpraia08@gmail.com>
2025-05-28 22:45:05 -0400

    Learned Caching. When to cache, when not to cache. Cacheable, EnableCaching, CacheEvict and CachePut along with scheduled caching

--- src/main/java/com/example/nobs/product/services/UpdateProductService.java
+++ src/main/java/com/example/nobs/product/services/UpdateProductService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,7 @@ public class UpdateProductService implements Command<UpdateProductCommand, Produ
     }
     
     @Override
+    @CachePut(value = "productCache", key="#command.getId()")
     public ResponseEntity<ProductDTO> execute(UpdateProductCommand command) {
         
         Optional<Product> productOptional = productRepository.findById(command.getId());
